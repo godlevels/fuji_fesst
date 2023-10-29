1dbe9366fc2d66b6f48b5367d6513adab38cf8d4
CryptoVmD <93841754+godlevels@users.noreply.github.com>
2023-10-29 21:03:27 +0100

    tst

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,5 @@
-/* eslint-disable react/jsx-no-target-blank */
 import Nav from './Nav'
-import heroImage from "../assets/img.png"
+// import heroImage from "../assets/img.png"
 import Hero_txt from "../assets/ibd.png"
 
 const Hero = () => {
@@ -9,7 +8,7 @@ const Hero = () => {
           <Nav />
 
           <div className='relative'>
-            <img src={heroImage} alt="" className='h-[60vh] md:h-screen w-full object-cover' />
+            {/* <img src={heroImage} alt="" className='h-[60vh] md:h-screen w-full object-cover' /> */}
 
             <div className='absolute inset-0 flex flex-col justify-center items-center mt-[5rem] md:mt-[3rem]'>
               <img src={Hero_txt} alt="" className='w-[10rem] md:w-[25rem] md:mb-[-3rem]'/>
@@ -18,7 +17,7 @@ const Hero = () => {
 
                 <div className='flex gap-x-5 justify-center items-center mt-5'>
                   <button className='bg-inherit border border-primary text-white text-[1rem] font-medium rounded-lg px-7 py-3 hover:bg-primary hover:text-black ease-linear duration-200'>Get Ticket</button>
-                  <a href="https://wa.link/akus0p" target="_blank">
+                  <a href="https://wa.link/akus0p" target="_blank" rel='noreferrer'>
                     <button className='bg-primary border border-primary bg-inherit text-black text-[1rem] font-medium rounded-lg px-7 py-3 hover:bg-inherit hover:text-white ease-linear duration-200'>Sponsor</button>
                   </a>
                 </div>
